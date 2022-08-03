d1788168731342ec378ec22ead48733d264e66a4
Kirill <apatski94@gmail.com>
2022-08-03 18:56:23 +0300

    get request / Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
+
+type getType = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [get, setGet] = useState<getType[]>([]);
+    console.log(get)
+    const cleanPageHandler = () => {
+        setGet([])
+    }
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setGet(json))
+    }, [])
+
+    return (
+        <div className="App">
+            <Button nickName="CleanPage" callBack={cleanPageHandler}/>
+            <ul>
+
+                {
+                    get.map(el => {
+                        return (
+                            <li key={el.id}>
+                                <div>{el.id}</div>
+                                <span>{el.userId}</span>
+                                <span>{el.title}</span>
+                            </li>
+                    )
+                    })
+                }
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
